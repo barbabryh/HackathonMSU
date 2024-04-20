38f290d33697b608e744f8a7f71c908022a2c3c3
kudryashhh <anya.kudryashova.05@mail.ru>
2024-04-20 16:59:29 +0300

    navbar

--- java/js.js
+++ java/js.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
-    $('load.html').click (function (e) {
-        e.preventDefault(); //will stop the link href to call the blog page
-     
-        setTimeout(function () {
-            window.location.href = "main.html"; //will redirect to your blog page (an ex: blog.html)
-         }, 2000); //will call the function after 2 secs.
-     
-     });
+    $(".hover_green").addClass("sectionnone");
+    $("#schedule").click(function(){
+        $(".hover_green").addClass("sectionblock");
+    });
 })
